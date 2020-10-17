6ddbd2afa29fd44007d596f4bba4be62977fd4fc
Jack Baron <jackmbaron@gmail.com>
2020-10-17 06:06:15 +0100

    register env vars

---
+++ lib/env/index.ts
@@ -0,0 +1,12 @@
+import { registerString } from './register'
+
+// #region Globals
+const NODE_ENV = registerString('NODE_ENV')
+const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
+export const IS_DEV = !IS_PROD
+// #endregion
+
+// #region Tokens
+export const STEAM_KEY = registerString('STEAM_KEY', true)
+export const DISCORD_TOKEN = registerString('DISCORD_TOKEN', true)
+// #endregion
